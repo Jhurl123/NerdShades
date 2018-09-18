6b1813417ee460ca506d8acf4c536614bb06a854
Jhurl123 <34147439+Jhurl123@users.noreply.github.com>
2018-09-18 15:13:10 -0400

    Update connectScr.php
    Edited to remove password to database

--- PHP/connectScr.php
+++ PHP/connectScr.php
@@ -14,11 +14,7 @@ class ConnectScr
    	
 	   else {
 	
-	       $servername= "localhost";
-           $username= "root";
-           $password= "";
-           $dbname = "myDBPDO";
-	
+	//Edited to remove password and admin
 	
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
